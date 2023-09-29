b435281b8acc90d54e88f9ab2105ef704955e802
Ken Kousen <ken.kousen@kousenit.com>
2023-09-29 15:21:17 -0400

    Add OpenAI client with ChatGPT, Dall-E, and Whisper transcription services
    Added a Spring Boot application that illustrates how to use the OpenAI calls from Java. Included is an OpenAI client service, Spring configuration for WebClient, and models for JSON structures used in OpenAI API.
    
    Updated build.gradle with required dependencies and application properties. Handling of images is done with FileUtils.
    
    This application will be useful for developers needing to interact with OpenAI from a Java/Spring Boot context.

--- src/main/java/com/kousenit/openaiclient/json/TranscriptionResponse.java
+++
@@ -1,4 +0,0 @@
-package com.kousenit.openaiclient.json;
-
-public record TranscriptionResponse(String text) {
-}
--- src/test/java/com/kousenit/openaiclient/services/OpenAIInterfaceTest.java
+++ src/test/java/com/kousenit/openaiclient/services/OpenAIInterfaceTest.java
@@ -5,22 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
